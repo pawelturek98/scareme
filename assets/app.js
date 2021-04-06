e42c267d685177f9776261315c1bf075c38603b1
pawix82 <pawelturek98@gmail.com>
2021-04-06 14:28:12 +0200

    articles and blog view

--- assets/app.js
+++ assets/app.js
@@ -19,45 +19,18 @@ window.onload = (e) => {
     let menu_item = document.getElementById('main-navigation');
     let menu_item_clone = menu_item.cloneNode(true);
     let fixed_menu = document.getElementById('fixed-navbar');
-
-    let load_sticky_menu = (status = 'load') => {
-        if(status == 'load') {
-            fixed_menu.appendChild(menu_item_clone);
-            console.log('sticky loaded');
-        } else {
-            menu_item_clone.parentNode.removeChild(menu_item_clone);
-            console.log('sticky unloaded');
-        }
-    }
-
-    if(window.innerWidth >= 1200) {
+    
+    if(window.innerWidth >= 992) {
         let sticky_loaded = false;
         window.onscroll = () => {
-            if(window.pageYOffset > 120) {
+            if(window.pageYOffset > menu_item.offsetTop) {
                 if(!sticky_loaded) {
-                    load_sticky_menu();
+                    fixed_menu.appendChild(menu_item_clone);
                     sticky_loaded = true;
                 }
             } else {
                 if(sticky_loaded) {
-                    load_sticky_menu('delete');
-                    sticky_loaded = false;
-                }
-            }
-        }
-    }
-
-    if(window.innerWidth >= 992 && window.innerWidth <= 1199) {
-        let sticky_loaded = false;
-        window.onscroll = () => {
-            if(window.pageYOffset > 350) {
-                if(!sticky_loaded) {
-                    load_sticky_menu();
-                    sticky_loaded = true;
-                }
-            } else {
-                if(sticky_loaded) {
-                    load_sticky_menu('delete');
+                    menu_item_clone.parentNode.removeChild(menu_item_clone);
                     sticky_loaded = false;
                 }
             }
